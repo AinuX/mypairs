8db2ecdf373b691e30c873cfb217a7b383285d56
podhmo <podhmo+altair@beproud.jp>
2014-11-02 00:25:34 +0900

    instance variables are also ok

--- mypairs/__init__.py
+++ mypairs/__init__.py
@@ -41,51 +41,51 @@ class all_pairs2(object):
             if not len(arr):
                 raise Exception("option arrays must have at least one item")
 
-        self.__filter_func = filter_func
-        self.__n = n
-        self.__pairs = s.PairsStorage(n)
-        self.__max_unique_pairs_expected = get_max_comb_number(options, n)
-        self.__working_arr = []
+        self.filter_func = filter_func
+        self.n = n
+        self.pairs = s.PairsStorage(n)
+        self.max_unique_pairs_expected = get_max_comb_number(options, n)
+        self.working_arr = []
 
         for i in range(len(options)):
-            self.__working_arr.append([item("a%iv%i" % (i, j), value) for j, value in enumerate(options[i])])
+            self.working_arr.append([item("a%iv%i" % (i, j), value) for j, value in enumerate(options[i])])
 
         for arr in previously_tested:
             if len(arr) == 0:
                 continue
-            elif len(arr) != len(self.__working_arr):
+            elif len(arr) != len(self.working_arr):
                 raise Exception("previously tested combination is not complete")
-            if not self.__filter_func(arr):
+            if not self.filter_func(arr):
                 raise Exception("invalid tested combination is provided")
             tested = []
             for i, val in enumerate(arr):
-                idxs = [item(node.id, 0) for node in self.__working_arr[i] if node.value == val]
+                idxs = [item(node.id, 0) for node in self.working_arr[i] if node.value == val]
                 if len(idxs) != 1:
                     raise Exception("value from previously tested combination is not found in the options or found more than once")
                 tested.append(idxs[0])
-            self.__pairs.add_sequence(tested)
+            self.pairs.add_sequence(tested)
 
     def next(self):
-        assert(len(self.__pairs) <= self.__max_unique_pairs_expected)
-        p = self.__pairs
-        if len(self.__pairs) == self.__max_unique_pairs_expected:
+        assert(len(self.pairs) <= self.max_unique_pairs_expected)
+        p = self.pairs
+        if len(self.pairs) == self.max_unique_pairs_expected:
             # no reasons to search further - all pairs are found
             raise StopIteration
 
-        previous_unique_pairs_count = len(self.__pairs)
-        chosen_values_arr = [None] * len(self.__working_arr)
-        indexes = [None] * len(self.__working_arr)
+        previous_unique_pairs_count = len(self.pairs)
+        chosen_values_arr = [None] * len(self.working_arr)
+        indexes = [None] * len(self.working_arr)
 
         direction = 1
         i = 0
 
-        while -1 < i < len(self.__working_arr):
+        while -1 < i < len(self.working_arr):
             if direction == 1:  # move forward
                 self.resort_working_array(chosen_values_arr[:i], i)
                 indexes[i] = 0
             elif direction == 0 or direction == -1:  # scan current array or go back
                 indexes[i] += 1
-                if indexes[i] >= len(self.__working_arr[i]):
+                if indexes[i] >= len(self.working_arr[i]):
                     direction = -1
                     if i == 0:
                         raise StopIteration
@@ -95,21 +95,21 @@ class all_pairs2(object):
             else:
                 raise Exception("next(): unknown 'direction' code.")
 
-            chosen_values_arr[i] = self.__working_arr[i][indexes[i]]
+            chosen_values_arr[i] = self.working_arr[i][indexes[i]]
 
-            if self.__filter_func(self.get_values_array(chosen_values_arr[:i + 1])):
+            if self.filter_func(self.get_values_array(chosen_values_arr[:i + 1])):
                 assert(direction > -1)
                 direction = 1
             else:
                 direction = 0
             i += direction
 
-        if len(self.__working_arr) != len(chosen_values_arr):
+        if len(self.working_arr) != len(chosen_values_arr):
             raise StopIteration
 
-        self.__pairs.add_sequence(chosen_values_arr)
+        self.pairs.add_sequence(chosen_values_arr)
 
-        if len(self.__pairs) == previous_unique_pairs_count:
+        if len(self.pairs) == previous_unique_pairs_count:
             # could not find new unique pairs - stop
             raise StopIteration
 
@@ -122,13 +122,13 @@ class all_pairs2(object):
         return [item.value for item in arr]
 
     def resort_working_array(self, chosen_values_arr, num):
-        for item in self.__working_arr[num]:
-            data_node = self.__pairs.get_node_info(item)
+        for item in self.working_arr[num]:
+            data_node = self.pairs.get_node_info(item)
 
             new_combs = []
-            for i in range(0, self.__n):
+            for i in range(0, self.n):
                 # numbers of new combinations to be created if this item is appended to array
-                new_combs.append(set([z for z in xunique_combinations(chosen_values_arr + [item], i + 1)]) - self.__pairs.get_combs()[i])
+                new_combs.append(set([z for z in xunique_combinations(chosen_values_arr + [item], i + 1)]) - self.pairs.get_combs()[i])
             # weighting the node
             item.weights = [-len(new_combs[-1])]    # node that creates most of new pairs is the best
             item.weights += [len(data_node.out)]  # less used outbound connections most likely to produce more new pairs while search continues
@@ -136,10 +136,10 @@ class all_pairs2(object):
             item.weights += [-data_node.counter]  # less used node is better
             item.weights += [-len(data_node.in_)]  # otherwise we will prefer node with most of free inbound connections; somehow it works out better ;)
 
-        self.__working_arr[num].sort(key=lambda a: a.weights)
+        self.working_arr[num].sort(key=lambda a: a.weights)
 
     # statistics, internal stuff
     def get_pairs_found(self):
-        return self.__pairs
+        return self.pairs
 
 __export__ = [all_pairs2, get_max_comb_number]
--- mypairs/structure.py
+++ mypairs/structure.py
@@ -12,47 +12,47 @@ class Node(object):
         self.out = set()
 
     def __str__(self):
-        return str(self.__dict__)
+        return str(self.dict__)
 
 
 class PairsStorage(object):
     def __init__(self, n):
-        self.__n = n
-        self.__nodes = {}
-        self.__combs_arr = []
+        self.n = n
+        self.nodes = {}
+        self.combs_arr = []
         for i in range(n):
-            self.__combs_arr.append(set())
+            self.combs_arr.append(set())
 
     def add(self, comb):
         n = len(comb)
         assert(n > 0)
 
-        self.__combs_arr[n - 1].add(comb)
-        if n == 1 and comb[0].id not in self.__nodes:
-            self.__nodes[comb[0].id] = Node(comb[0].id)
+        self.combs_arr[n - 1].add(comb)
+        if n == 1 and comb[0].id not in self.nodes:
+            self.nodes[comb[0].id] = Node(comb[0].id)
             return
 
         ids = [x.id for x in comb]
         for i, id in enumerate(ids):
-            curr = self.__nodes[id]
+            curr = self.nodes[id]
             curr.counter += 1
             curr.in_.update(ids[:i])
             curr.out.update(ids[i + 1:])
 
     def add_sequence(self, seq):
-        for i in range(1, self.__n + 1):
+        for i in range(1, self.n + 1):
             for comb in xunique_combinations(seq, i):
                 self.add(comb)
 
     def get_node_info(self, item):
-        return self.__nodes.get(item.id, Node(item.id))
+        return self.nodes.get(item.id, Node(item.id))
 
     def get_combs(self):
-        return self.__combs_arr
+        return self.combs_arr
 
     def __len__(self):
-        return len(self.__combs_arr[-1])
+        return len(self.combs_arr[-1])
 
     def count_new_combs(self, seq):
-        s = set([z for z in xunique_combinations(seq, self.__n)]) - self.__combs_arr[-1]
+        s = set([z for z in xunique_combinations(seq, self.n)]) - self.combs_arr[-1]
         return len(s)
